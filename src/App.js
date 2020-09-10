482e7af962ef331f10773664e2373a55a53854c1
peachykish <nakisha.carroll@gmail.com>
2020-09-10 15:31:16 -0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To Do Today</h1>
+      <input type="text"></input>
+      <button> Add Item</button>
+        <ul>
+          <li>Go to the store</li>
+          <li>Do Laundry</li>
+          <li>Go to the gym</li>
+          <li>Take a nap</li> 
+        </ul>
+        <nav>
+          <img alt="cat" src="./profile.png"/>
+          <ul>
+          <li>To Do</li>
+          <li>Dashboard</li>
+          <li>Games</li>
+          </ul>
+        </nav>
     </div>
   );
 }
